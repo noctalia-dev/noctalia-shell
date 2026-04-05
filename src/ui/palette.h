740459a8d3f9f786c5e382d2c4ea8b8914331361
Lemmy <studio@quadbyte.net>
2026-04-05 09:41:45 -0400

    feat(ui): changed dev palette to rosepine

--- src/ui/palette.h
+++ src/ui/palette.h
@@ -21,21 +21,42 @@ struct Palette {
   Color onHover;
 };
 
+// Rosepine
 inline constexpr Palette palette{
-    .primary = hex("#fff59b"),
-    .onPrimary = hex("#0e0e43"),
-    .secondary = hex("#a9aefe"),
-    .onSecondary = hex("#0e0e43"),
-    .tertiary = hex("#9BFECE"),
-    .onTertiary = hex("#0e0e43"),
-    .error = hex("#FD4663"),
-    .onError = hex("#0e0e43"),
-    .surface = hex("#070722"),
-    .onSurface = hex("#f3edf7"),
-    .surfaceVariant = hex("#11112d"),
-    .onSurfaceVariant = hex("#7c80b4"),
-    .outline = hex("#21215F"),
-    .shadow = hex("#070722"),
-    .hover = hex("#9BFECE"),
-    .onHover = hex("#0e0e43"),
+    .primary = hex("#ebbcba"),
+    .onPrimary = hex("#191724"),
+    .secondary = hex("#9ccfd8"),
+    .onSecondary = hex("#191724"),
+    .tertiary = hex("#31748f"),
+    .onTertiary = hex("#e0def4"),
+    .error = hex("#eb6f92"),
+    .onError = hex("#191724"),
+    .surface = hex("#191724"),
+    .onSurface = hex("#e0def4"),
+    .surfaceVariant = hex("#26233a"),
+    .onSurfaceVariant = hex("#908caa"),
+    .outline = hex("#403d52"),
+    .shadow = hex("#191724"),
+    .hover = hex("#524f67"),
+    .onHover = hex("#e0def4"),
 };
+
+// Noctalia
+// inline constexpr Palette palette{
+//     .primary = hex("#fff59b"),
+//     .onPrimary = hex("#0e0e43"),
+//     .secondary = hex("#a9aefe"),
+//     .onSecondary = hex("#0e0e43"),
+//     .tertiary = hex("#9BFECE"),
+//     .onTertiary = hex("#0e0e43"),
+//     .error = hex("#FD4663"),
+//     .onError = hex("#0e0e43"),
+//     .surface = hex("#070722"),
+//     .onSurface = hex("#f3edf7"),
+//     .surfaceVariant = hex("#11112d"),
+//     .onSurfaceVariant = hex("#7c80b4"),
+//     .outline = hex("#21215F"),
+//     .shadow = hex("#070722"),
+//     .hover = hex("#9BFECE"),
+//     .onHover = hex("#0e0e43"),
+// };
