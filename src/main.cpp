5e88de526d87e1b6c6494e2b9eec3553c3279dd4
Lemmy <studio@quadbyte.net>
2026-04-05 23:52:26 -0400

    feat(app): minimal --help and --version

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include "app/application.h"
 #include "core/log.h"
 
+#include <cstring>
 #include <stdexcept>
 
-int main() {
+#ifndef NOCTALIA_VERSION
+#define NOCTALIA_VERSION "unknown"
+#endif
+
+int main(int argc, char* argv[]) {
+  for (int i = 1; i < argc; ++i) {
+    if (std::strcmp(argv[i], "--version") == 0) {
+      std::puts("noctalia " NOCTALIA_VERSION);
+      return 0;
+    }
+    if (std::strcmp(argv[i], "--help") == 0) {
+      std::puts("Usage: noctalia [OPTIONS]\n"
+                "\n"
+                "Options:\n"
+                "  --help     Show this help message\n"
+                "  --version  Show version information");
+      return 0;
+    }
+  }
+
   try {
     Application app;
     app.run();
