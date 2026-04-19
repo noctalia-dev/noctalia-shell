a957fe8cc4c85e18e3c00be27ad0cc45a21c2018
Linus Ammon <235536459+linusammon@users.noreply.github.com>
2026-04-19 13:44:29 +0200

    refactor(nixos-module): make systemd service optional

--- nix/nixos-module.nix
+++ nix/nixos-module.nix
@@ -8,28 +8,32 @@ let
 in
 {
   options.services.noctalia-shell = {
-    enable = lib.mkEnableOption "Noctalia shell systemd service";
+    enable = lib.mkEnableOption "Noctalia shell";
 
     package = lib.mkOption {
       type = lib.types.package;
       description = "The noctalia-shell package to use";
     };
 
-    target = lib.mkOption {
-      type = lib.types.str;
-      default = "graphical-session.target";
-      example = "hyprland-session.target";
-      description = "The systemd target for the noctalia-shell service.";
+    systemd = {
+      enable = lib.mkEnableOption "Noctalia shell systemd service";
+
+      target = lib.mkOption {
+        type = lib.types.str;
+        default = "graphical-session.target";
+        example = "hyprland-session.target";
+        description = "The systemd target for the noctalia-shell service.";
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
-    systemd.user.services.noctalia-shell = {
+    systemd.user.services.noctalia-shell = lib.mkIf cfg.systemd.enable {
       description = "Noctalia Shell - Wayland desktop shell";
       documentation = [ "https://docs.noctalia.dev" ];
-      after = [ cfg.target ];
-      partOf = [ cfg.target ];
-      wantedBy = [ cfg.target ];
+      after = [ cfg.systemd.target ];
+      partOf = [ cfg.systemd.target ];
+      wantedBy = [ cfg.systemd.target ];
       restartTriggers = [ cfg.package ];
 
       environment = {
