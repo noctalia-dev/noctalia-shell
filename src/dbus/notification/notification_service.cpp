fc3c82432c2bfc14329a6ac7129f765940a2bdba
Lemmy <studio@quadbyte.net>
2026-04-24 19:35:22 -0400

    chore(version): setting to 5.0.0

--- src/dbus/notification/notification_service.cpp
+++ src/dbus/notification/notification_service.cpp
@@ -280,5 +280,5 @@ void NotificationService::emitActionInvoked(uint32_t id, const std::string& acti
 }
 
 std::tuple<std::string, std::string, std::string, std::string> NotificationService::onGetServerInformation() {
-  return {"noctalia", "noctalia-dev", "0.1.0", "1.2"};
+  return {"noctalia", "noctalia-dev", NOCTALIA_VERSION, "1.2"};
 }
