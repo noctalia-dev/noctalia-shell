dc9f5467695a369b418c493a44aaa787c0e95b9f
Rain Xelelo <rXelelo@outlook.com>
2026-04-28 19:56:44 +0300

    Fix arch type build cleanup

--- flake.nix
+++ flake.nix
@@ -3,107 +3,124 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages.default = pkgs.stdenv.mkDerivation {
-          pname = "noctalia";
-          version = "5.0.0";
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      systems = [
+        "x86_64-linux"
+      ];
+      forEachSystem = perSystem:
+        lib.genAttrs systems (
+          system: let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+            perSystem { inherit pkgs system; }
+        );
+    in
+    {
+      packages = forEachSystem (
+        { pkgs, ... }: {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "noctalia";
+            version = "5.0.0";
 
-          src = ./.;
+            src = ./.;
 
-          nativeBuildInputs = with pkgs; [
-            meson
-            ninja
-            pkg-config
-            wayland-scanner
-          ];
+            nativeBuildInputs = with pkgs; [
+              meson
+              ninja
+              pkg-config
+              wayland-scanner
+            ];
 
-          buildInputs = with pkgs; [
-            wayland
-            wayland-protocols
-            libGL
-            libglvnd
-            freetype
-            fontconfig
-            cairo
-            pango
-            libxkbcommon
-            sdbus-cpp_2
-            systemd
-            pipewire
-            pam
-            curl
-            libwebp
-          ];
+            buildInputs = with pkgs; [
+              wayland
+              wayland-protocols
+              libGL
+              libglvnd
+              freetype
+              fontconfig
+              cairo
+              pango
+              libxkbcommon
+              sdbus-cpp_2
+              systemd
+              pipewire
+              pam
+              curl
+              libwebp
+            ];
 
-          mesonBuildType = "release";
-          
-          dontUseMesonConfigure = true;
-          
-          configurePhase = ''
-            runHook preConfigure
-            meson setup build --prefix=$out --buildtype=release
-            runHook postConfigure
-          '';
-          
-          buildPhase = ''
-            runHook preBuild
-            meson compile -C build
-            runHook postBuild
-          '';
-          
-          installPhase = ''
-            runHook preInstall
-            meson install -C build
-            runHook postInstall
-          '';
+            mesonBuildType = "release";
+            
+            dontUseMesonConfigure = true;
+            
+            configurePhase = ''
+              runHook preConfigure
+              meson setup build --prefix=$out --buildtype=release
+              runHook postConfigure
+            '';
+            
+            buildPhase = ''
+              runHook preBuild
+              meson compile -C build
+              runHook postBuild
+            '';
+            
+            installPhase = ''
+              runHook preInstall
+              meson install -C build
+              runHook postInstall
+            '';
 
-          meta = with pkgs.lib; {
-            description = "A lightweight Wayland shell and bar built directly on Wayland + OpenGL ES";
-            homepage = "https://github.com/anomalyco/noctalia-shell";
-            license = licenses.mit;
-            platforms = platforms.linux;
-            mainProgram = "noctalia";
+            meta = with pkgs.lib; {
+              description = "A lightweight Wayland shell and bar built directly on Wayland + OpenGL ES";
+              homepage = "https://github.com/anomalyco/noctalia-shell";
+              license = licenses.mit;
+              platforms = platforms.linux;
+              mainProgram = "noctalia";
+            };
           };
-        };
+        }
+      );
 
-        # Development shell with all dependencies
-        # Use this for development: nix develop
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.default ];
-          
-          buildInputs = with pkgs; [
-            just
-            clang-tools
-            gdb
-          ];
+      devShells = forEachSystem (
+        { pkgs, system }: {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.default ];
+            
+            buildInputs = with pkgs; [
+              just
+              clang-tools
+              gdb
+            ];
 
-          shellHook = ''
-            echo "Noctalia development environment"
-            echo ""
-            echo "Available commands:"
-            echo "  just configure       - Configure debug build"
-            echo "  just build           - Build debug"
-            echo "  just run             - Run debug build"
-            echo "  just configure release - Configure release build"
-            echo "  just build release   - Build release"
-            echo "  just run release     - Run release build"
-            echo ""
-            echo "Note: Use 'just build' instead of 'nix build' due to sdbus-c++ API compatibility"
-          '';
-        };
+            shellHook = ''
+              echo "Noctalia development environment"
+              echo ""
+              echo "Available commands:"
+              echo "  just configure       - Configure debug build"
+              echo "  just build           - Build debug"
+              echo "  just run             - Run debug build"
+              echo "  just configure release - Configure release build"
+              echo "  just build release   - Build release"
+              echo "  just run release     - Run release build"
+              echo ""
+              echo "Note: Use 'just build' instead of 'nix build' due to sdbus-c++ API compatibility"
+            '';
+          };
+        }
+      );
 
-        apps.default = {
-          type = "app";
-          program = "${self.packages.${system}.default}/bin/noctalia";
-        };
-      }
-    );
+      apps = forEachSystem (
+        { system, ... }: {
+          default = {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/noctalia";
+          };
+        }
+      );
+    };
 }
